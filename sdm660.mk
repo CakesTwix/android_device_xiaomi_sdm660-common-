3f866d1f9b6fb349586cdd6430fccea5aa6e3685
Alcatraz323 <alcatraz32323@gmail.com>
2024-03-10 14:51:50 +0800

    sdm660-common: move RIL related config to specific overlay

--- sdm660.mk
+++ sdm660.mk
@@ -390,6 +390,8 @@ PRODUCT_PACKAGES += \
     SDM660CarrierConfigOverlay \
     SDM660DialerOverlay \
     SDM660FrameworksOverlay \
+    SDM660RILFrameworksOverlay \
+    SDM660RILSettingsOverlay \
     SDM660SettingsOverlay \
     SDM660SettingsProviderOverlay \
     SDM660SystemUIOverlay \
